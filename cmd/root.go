1f39ba82671cc168ef1bf3b965dd2b13b5dfee7f
Vibhav Bobade <vibhav.bobde@gmail.com>
2019-09-02 01:45:05 +0530

    Added function for making helm charts usable for kustomize

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,18 @@
 package cmd
 
 import (
-	"fmt"
 
-	"sigs.k8s.io/kustomize/pkg/commands/build"
+	//"sigs.k8s.io/kustomize/pkg/commands/build"
+
+	"bufio"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"regexp"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -19,7 +28,74 @@ var RootCmd = &cobra.Command{
  to quickly create a Cobra application.`,
 
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello Kust !")
-		build.NewOptions("", "")
+		if len(args) > 0 {
+			cwd, err := os.Getwd()
+			if err != nil {
+				panic(err)
+			}
+			chartPath := path.Join(cwd, args[0])
+			templatesPath := path.Join(chartPath, "templates")
+			templates, err := ioutil.ReadDir(templatesPath)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, t := range templates {
+				processedTemplate := []string{}
+				if strings.HasSuffix(t.Name(), ".yaml") {
+					templatePath := path.Join(templatesPath, t.Name())
+					/*
+						1st Stage
+						Convert all the golang templating stuff to strings
+					*/
+					processedTemplate = helmTemplate2KelpTemplate(templatePath)
+					/*
+						2nd Stage
+						Override with kustumization values
+					*/
+					//processedTemplate = kelpApplyKustomization(processedTemplate)
+					/*
+						3rd Stage
+						Convert back to helm template
+					*/
+				}
+				for _, line := range processedTemplate {
+					fmt.Println(line)
+				}
+			}
+
+		} else {
+			panic(errors.New("No arguments given"))
+		}
+
+		//fmt.Println("Checking for kustomization file")
+		//build.NewOptions("", "")
 	},
+}
+
+func helmTemplate2KelpTemplate(templatePath string) []string {
+	var processedTemplate = []string{}
+	template, err := os.Open(templatePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer template.Close()
+
+	scanner := bufio.NewScanner(template)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if check, _ := regexp.MatchString("^{{", line); check == true {
+		} else if check, _ := regexp.MatchString("\"{{.*(}})?({{)?.*}}\"", line); check == false {
+			if check, _ := regexp.MatchString("{{.*(}})?({{)?.*}}", line); check == true {
+				line = strings.Replace(line, "{{", "\"{{", -1)
+				line = strings.Replace(line, "}}", "}}\"", -1)
+			}
+		}
+		processedTemplate = append(processedTemplate, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return processedTemplate
 }
